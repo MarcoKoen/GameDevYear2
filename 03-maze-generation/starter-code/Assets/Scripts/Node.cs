40e3b0fda1384f9d36e1e0d8e86ade18aa081a9f
Marco Koen <koenmt1@student.op.ac.nz>
2022-06-21 15:06:23 +1200

    Added Node.cs code comment

--- 03-maze-generation/starter-code/Assets/Scripts/Node.cs
+++ 03-maze-generation/starter-code/Assets/Scripts/Node.cs
@@ -1,3 +1,14 @@
+/* Program name: Maze Generation
+   Project file name: Node.cs
+   Author: Marco Koen
+   Date: 14/06/2022
+   Language: C#
+   Platform: Windows
+   Purpose: A utility class of node
+   Description: Creates nodes and sets their x and y + other settings. Allows us to create our paths.
+   Known Bugs:
+   Additional Features:
+*/
 public class Node
 {
     public int x;
