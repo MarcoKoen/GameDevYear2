7c37373301fcc5dacf46d0d51825744b5f8aba65
Marco Koen <koenmt1@student.op.ac.nz>
2022-06-22 11:57:12 +1200

    Added code comment to MoveData.cs

--- 04-ai-strategy/starter code/Assets/Scripts/MoveData.cs
+++ 04-ai-strategy/starter code/Assets/Scripts/MoveData.cs
@@ -1,4 +1,16 @@
-﻿using System.Collections;
+﻿/* Program name: 04-ai-strategy
+   Project file name: MoveData.cs
+   Author: Marco Koen
+   Date: 20/06/2022
+   Language: C#
+   Platform: Windows
+   Purpose: Holds the move data?
+   Description: 
+   Known Bugs:
+   Additional Features:
+*/
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
